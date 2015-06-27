b6473881512b1416476390f88264a06fc97e318c
Darko Sancanin <darko.sancanin@gmail.com>
2015-06-28 09:48:23 +1000

    Initial commit.

---
+++ app/src/main/java/com/darkosancanin/naturaldateandtime/ExamplesActivity.java
@@ -0,0 +1,13 @@
+package com.darkosancanin.naturaldateandtime;
+
+import android.os.Bundle;
+
+
+public class ExamplesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examples);
+    }
+}
---
+++ app/src/main/java/com/darkosancanin/naturaldateandtime/QuestionActivity.java
@@ -0,0 +1,13 @@
+package com.darkosancanin.naturaldateandtime;
+
+import android.os.Bundle;
+
+
+public class QuestionActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
