52f270b4212c072f29fada1d9bbf0f50244168c5
Darko Sancanin <darko.sancanin@gmail.com>
2015-07-03 19:23:36 +1000

    Added title to about and examples screens. (need to fix logo).

--- app/src/main/java/com/darkosancanin/naturaldateandtime/AboutActivity.java
+++ app/src/main/java/com/darkosancanin/naturaldateandtime/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.darkosancanin.naturaldateandtime;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class AboutActivity extends BaseActivity {
@@ -9,5 +13,25 @@ public class AboutActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setUpButtonClickEvents();
+    }
+
+    private void setUpButtonClickEvents(){
+        Button wwwButton = (Button) findViewById(R.id.www_button);
+        wwwButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naturaldateandtime.com"));
+                startActivity(browserIntent);
+            }
+        });
+
+        Button emailButton = (Button) findViewById(R.id.email_button);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"naturaldatetime@idarko.com"});
+                startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            }
+        });
     }
 }
